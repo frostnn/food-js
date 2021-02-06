fb6969f62bc2e2898b484cf512714718700be37b
frostnn <gradysnn92@yandex.ru>
2021-02-06 21:46:20 +0300

    add modal

--- js/timer.js
+++ js/timer.js
@@ -1,44 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-  /* tabs */
-
-  const tabs = document.querySelectorAll('.tabheader__item');
-  const tabsContent = document.querySelectorAll('.tabcontent');
-  const tabsParent = document.querySelector('.tabheader__items');
-
-  const hideTabContent = () => {
-    tabsContent.forEach((item) => {
-      item.classList.remove('show');
-      item.classList.add('hide', 'fade');
-    });
-    tabs.forEach((tab) => {
-      tab.classList.remove('tabheader__item_active');
-    });
-  };
-
-  const showeTabContent = (i = 0) => {
-    tabsContent[i].classList.remove('hide');
-    tabsContent[i].classList.add('show', 'fade');
-    tabs[i].classList.add('tabheader__item_active');
-  };
-
-  hideTabContent();
-  showeTabContent();
-
-  tabsParent.addEventListener('click', (e) => {
-    const target = e.target;
-    console.log(target);
-    if (target && target.classList.contains('tabheader__item')) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showeTabContent(i);
-        }
-      });
-    }
-  });
-
-  /* timer */
-
   const deadline = '2021-05-15';
 
   const getTimeRemaining = (endTime) => {
